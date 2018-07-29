07cad45da04e4e665aaf7e54b381a0ae302a52b6
Thanwarin Pisanprechatam <numberthl2ee@gmail.com>
2018-07-29 22:07:15 +0700

    implement: style and apply on index item componect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Indexes from './components/Indexes'
+import "typeface-roboto";
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
             "name" : "Now TV",
             "email" : "nowtv@test.com"
           },
+          "dateTime": "11:35",
           "subject" : "Grab another Pass, you need to be watching this...",
           "body" : "Oscar winners Sir Anthony Hopkins and Ed Harris join an impressive cast boasting the likes of Thandie Newton, James Marsden and Jeffrey Wright."
         },
@@ -21,6 +23,7 @@ class App extends Component {
             "name" : "Investopedia Terms",
             "email" : "investopedia@test.com"
           },
+          "dateTime": "Yesterday, 11:35",
           "subject" : "What is 'Fibonanci Retracement'?",
           "body" : "Fibonacci retracement is a term used in technical analysis that refers to areas of support (price stops going lower) or resistance (price stops going higher)."
         },
@@ -29,6 +32,7 @@ class App extends Component {
             "name" : "ASICS Greater Manchester Marathon ",
             "email" : "events@human-race.co.uk"
           },
+          "dateTime": "20/08/2018, 14:40",
           "subject" : "Your chance to take on the marathon",
           "body" : "Do you feel inspired to take on one of Europe's most highly regarded and popular marathons?"
         }
@@ -40,7 +44,8 @@ class App extends Component {
     let emailIndexes = this.state.emails.map((email) => ({
       name: email.from.name,
       title: email.subject,
-      description: email.description
+      description: email.body,
+      dateTime: email.dateTime
     }));
     return (
       <div className="App">
--- src/components/IndexItem.js
+++ src/components/IndexItem.js
@@ -4,10 +4,50 @@ class IndexItem extends Component {
   constructor() {
     super();
   }
+
   render() {
+    const getFirstCharacterFromString = (str) => (
+      (str || "").charAt(0)
+    );
+
+    let words = this.props.index.name.split(" ");
+    let label = getFirstCharacterFromString(
+      (!words) ? "" : words[0]
+    );
+    
+    if (words.length === 2) {
+      label = `${
+        getFirstCharacterFromString(words[0])
+      }${
+        getFirstCharacterFromString(words[1])
+      }`;
+    }
+
     return (
       <div className="Indexes">
-        {this.props.index.name} - {this.props.index.title} - {this.props.index.description}   
+        <div class="index-item">
+          <div class="index-item-icon">
+            <div class="index-item-icon-circle">
+              <div class="index-item-icon-circle-label">
+                {label}
+              </div>
+            </div> 
+          </div>
+          <div class="index-item-details">
+            <div class="index-item-details-name">
+              {this.props.index.name}
+            </div>
+            <div class="index-item-details-title">
+              {this.props.index.title}
+            </div>
+            <div class="index-item-details-time">
+              {this.props.index.dateTime}
+            </div>
+            <div class="index-item-details-description">
+              {this.props.index.description}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
