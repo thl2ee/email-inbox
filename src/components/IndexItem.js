8a1dca50901040b359f92b3381a9df56177bfc60
Thanwarin Pisanprechatam <numberthl2ee@gmail.com>
2018-07-31 09:50:36 +0700

    add: color list for background color of the index label

--- src/components/IndexItem.js
+++ src/components/IndexItem.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import '../styles/IndexItem.css';
 
-class IndexItem extends Component {
-  constructor() {
-    super();
-  }
+const COLOR_LIST = [
+  "rgb(95,65,183)",
+  "rgb(23,161,26)",
+  "rgb(125,57,119)",
+  "rgb(252,25,151)",
+  "rgb(46,88,149)"
+];
 
-  render() {
+class IndexItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: props.index
+    }
+  }
+  
+  render = () =>  {
     const getFirstCharacterFromString = (str) => (
       (str || "").charAt(0)
     );
-
-    let words = this.props.index.name.split(" ");
+    let words = this.state.index.name.split(" ");
     let label = getFirstCharacterFromString(
       (!words) ? "" : words[0]
     );
@@ -26,28 +36,36 @@ class IndexItem extends Component {
 
     return (
       <div className="Indexes">
-        <div class="index-item">
-          <div class="index-item-icon">
-            <div class="index-item-icon-circle">
-              <div class="index-item-icon-circle-label">
+        <div className="index-item">
+
+          <div className="index-item-icon">
+            <div className="index-item-icon-circle"
+              style={{ 
+                backgroundColor: COLOR_LIST[
+                  this.state.index.id % COLOR_LIST.length
+                ]
+              }} >
+              <div className="index-item-icon-circle-label">
                 {label}
               </div>
             </div> 
           </div>
-          <div class="index-item-details">
-            <div class="index-item-details-name">
-              {this.props.index.name}
+
+          <div className="index-item-details">
+            <div className="index-item-details-name">
+              {this.state.index.name}
             </div>
-            <div class="index-item-details-title">
-              {this.props.index.title}
+            <div className="index-item-details-title">
+              {this.state.index.title}
             </div>
-            <div class="index-item-details-time">
-              {this.props.index.dateTime}
+            <div className="index-item-details-time">
+              {this.state.index.dateTime}
             </div>
-            <div class="index-item-details-description">
-              {this.props.index.description}
+            <div className="index-item-details-description">
+              {this.state.index.description}
             </div>
           </div>
+          
         </div>
       </div>
     );
