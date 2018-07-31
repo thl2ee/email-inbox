2dee0fe173f493c76c0f1e7ff809ad01d6e29efa
Thanwarin Pisanprechatam <numberthl2ee@gmail.com>
2018-07-31 09:53:28 +0700

    add: lazy rendering for the index item component and add: new attributes for the wight and height of the component

--- src/components/Indexes.js
+++ src/components/Indexes.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import IndexItem from './IndexItem';
+const Infinite = require('react-infinite');
 
 class Indexes extends Component {
-  constructor() {
-    super();
-    this.state = {
-      indexes: []
-    }
-  }
-  render() {
-    let indexItems;
-    let indexes = this.props.indexes;
-    if (indexes) {
-      indexItems = indexes.map((index) => (
-        <IndexItem key={index.name} index={index} />
-      ));
-    }
+  render = () => {
     return (
-      <div className="Indexes">
-       {indexItems}
+      <div className="Indexes"
+        style={{
+          height: `${this.props.height}px` || "100%",
+          width: `${this.props.width}px` || "100%"
+        }}>
+        <Infinite 
+          containerHeight={520} 
+          elementHeight={100} >
+            {
+              this.props.indexes.map((index) => (
+                <IndexItem key={index.id} index={index} />
+              ))
+            }
+        </Infinite>
+        
       </div>
     );
   }
